7d337c96e26c70b88110cbfa4b6779410dffd0b8
dylanmmeza <dmm7036@psu.edu>
2020-09-01 20:29:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+temp = input("Enter temperature: ")
+sign = input("Enter unit in F/f or C/c:")
+temp = float (temp)
+if sign == 'F' or sign == 'f' :
+  Celsius  = (temp-32)/1.8
+  print(f"{temp}\N{degree sign} in Fahrenheit is equivalent to {Celsius }\N{degree sign} Celsius.")
+
+
+
+elif sign == 'C'or sign == 'c' :
+  fahrenheit  = (temp *1.8)+32
+  print(f"{temp}\N{degree sign} in Celsius is equivalent to {fahrenheit }\N{degree sign} Fahrenheit.")
+
+
+
+
