c0c38e99f9e92394bfa4282162f72795c88486e1
dylanmmeza <dmm7036@psu.edu>
2020-09-01 20:36:31 +0000

    else

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 temp = input("Enter temperature: ")
-sign = input("Enter unit in F/f or C/c:")
+sign = input("Enter unit in F/f or C/c: ")
 temp = float (temp)
 if sign == 'F' or sign == 'f' :
   Celsius  = (temp-32)/1.8
@@ -10,6 +10,11 @@ if sign == 'F' or sign == 'f' :
 elif sign == 'C'or sign == 'c' :
   fahrenheit  = (temp *1.8)+32
   print(f"{temp}\N{degree sign} in Celsius is equivalent to {fahrenheit }\N{degree sign} Fahrenheit.")
+
+else :
+   print(f"Invalid unit({sign})")
+
+
 
 
 
