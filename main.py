1869938f04dada40c2d970f0ff1fe661313f7548
dylanmmeza <dmm7036@psu.edu>
2020-09-02 18:08:33 +0000

    contributors and spacing

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+# Author: Dylan Meza dmm7036@psu.edu
+# Contributor: Soham Mishra svm6422@psu.edu
+# Contributor: Adnan Traore aqt5519@psu.edu
+# Contributor: Sydney Wetzel skw5571@psu.edu
+
+
 temp = input("Enter temperature: ")
 sign = input("Enter unit in F/f or C/c: ")
 temp = float (temp)
 if sign == 'F' or sign == 'f' :
   Celsius  = (temp-32)/1.8
   print(f"{temp}\N{degree sign} in Fahrenheit is equivalent to {Celsius }\N{degree sign} Celsius.")
-
-
-
+  
 elif sign == 'C'or sign == 'c' :
   fahrenheit  = (temp *1.8)+32
   print(f"{temp}\N{degree sign} in Celsius is equivalent to {fahrenheit }\N{degree sign} Fahrenheit.")
